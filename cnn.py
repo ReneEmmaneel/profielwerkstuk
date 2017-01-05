99be98ab96ae5a3366a91a3ff9bdd92ffa1173bf
Brian <brian.groskamp@icloud.com>
2017-01-05 17:51:24 +0100

    regels code verminderd door optimalisatie

--- cnn.py
+++ cnn.py
@@ -59,7 +59,8 @@ def benchmark(runs):
     starttime = time.time()
     for i in xrange(0, runs):
         cnn('dataset/0/img001-001.png', generateModel(), True)
-    print "--------------------------------------------------------\nhet convnet heeft over "+str(runs)+" runs " + str(time.time() - starttime)+ "seconde over gedaan"
+    now = time.time()
+    print "----------------------------------------------------------\nhet convnet heeft over "+str(runs)+" runs " + str(now - starttime)+ "s over gedaan\ndit geeft een gemiddelde van " + str((now - starttime)/runs) + "s per run"
 
 
 def cnn(imagePath, model, training=False):
@@ -85,12 +86,7 @@ def cnn(imagePath, model, training=False):
     featuremap1 = [ [], [], [], [], [], [] ]
     convmodel1 = model[0]
     for y in xrange(2, 30):
-        row1 = []
-        row2 = []
-        row3 = []
-        row4 = []
-        row5 = []
-        row6 = []
+        rows = [ [], [], [], [], [], [] ]
         for x in xrange(2, 30):
             receptivefield = np.asarray([
                 inputlayer[y-2][x-2], inputlayer[y-2][x-1], inputlayer[y-2][x], inputlayer[y-2][x+1], inputlayer[y-2][x+2],
@@ -99,18 +95,10 @@ def cnn(imagePath, model, training=False):
                 inputlayer[y+1][x-2], inputlayer[y+1][x-1], inputlayer[y+1][x], inputlayer[y+1][x+1], inputlayer[y+1][x+2],
                 inputlayer[y+2][x-2], inputlayer[y+2][x-1], inputlayer[y+2][x], inputlayer[y+2][x+1], inputlayer[y+2][x+2],
             ])
-            row1.append(max(0, np.sum(np.multiply(receptivefield, convmodel1[0]))))
-            row2.append(max(0, np.sum(np.multiply(receptivefield, convmodel1[1]))))
-            row3.append(max(0, np.sum(np.multiply(receptivefield, convmodel1[2]))))
-            row4.append(max(0, np.sum(np.multiply(receptivefield, convmodel1[3]))))
-            row5.append(max(0, np.sum(np.multiply(receptivefield, convmodel1[4]))))
-            row6.append(max(0, np.sum(np.multiply(receptivefield, convmodel1[5]))))
-        featuremap1[0].append(row1)
-        featuremap1[1].append(row2)
-        featuremap1[2].append(row3)
-        featuremap1[3].append(row4)
-        featuremap1[4].append(row5)
-        featuremap1[5].append(row6)
+            for i in xrange(0, 6):
+                rows[i].append(max(0, np.sum(np.multiply(receptivefield, convmodel1[i]))))
+        for h in xrange(0, 6):
+            featuremap1[h].append(rows[h])
     featuremap1 = np.asarray(featuremap1)
 
 
@@ -140,22 +128,7 @@ def cnn(imagePath, model, training=False):
     featuremap2 = [ [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []  ]
     convmodel2 = model[1]
     for y in xrange(2, 12):
-        row1 = []
-        row2 = []
-        row3 = []
-        row4 = []
-        row5 = []
-        row6 = []
-        row7 = []
-        row8 = []
-        row9 = []
-        row10 = []
-        row11 = []
-        row12 = []
-        row13 = []
-        row14 = []
-        row15 = []
-        row16 = []
+        rows = [ [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []  ]
         for x in xrange(2, 12):
             receptivefield = np.asarray([
                 maxpooled1[0][y-2][x-2], maxpooled1[0][y-2][x-1], maxpooled1[0][y-2][x], maxpooled1[0][y-2][x+1], maxpooled1[0][y-2][x+2],
@@ -194,38 +167,10 @@ def cnn(imagePath, model, training=False):
                 maxpooled1[5][y+1][x-2], maxpooled1[5][y+1][x-1], maxpooled1[5][y+1][x], maxpooled1[5][y+1][x+1], maxpooled1[5][y+1][x+2],
                 maxpooled1[5][y+2][x-2], maxpooled1[5][y+2][x-1], maxpooled1[5][y+2][x], maxpooled1[5][y+2][x+1], maxpooled1[5][y+2][x+2],
             ])
-            row1.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[0]))))
-            row2.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[1]))))
-            row3.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[2]))))
-            row4.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[3]))))
-            row5.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[4]))))
-            row6.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[5]))))
-            row7.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[6]))))
-            row8.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[7]))))
-            row9.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[8]))))
-            row10.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[9]))))
-            row11.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[10]))))
-            row12.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[11]))))
-            row13.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[12]))))
-            row14.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[13]))))
-            row15.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[14]))))
-            row16.append(max(0, np.sum(np.multiply(receptivefield, convmodel2[15]))))
-        featuremap2[0].append(row1)
-        featuremap2[1].append(row2)
-        featuremap2[2].append(row3)
-        featuremap2[3].append(row4)
-        featuremap2[4].append(row5)
-        featuremap2[5].append(row6)
-        featuremap2[6].append(row7)
-        featuremap2[7].append(row8)
-        featuremap2[8].append(row9)
-        featuremap2[9].append(row10)
-        featuremap2[10].append(row11)
-        featuremap2[11].append(row12)
-        featuremap2[12].append(row13)
-        featuremap2[13].append(row14)
-        featuremap2[14].append(row15)
-        featuremap2[15].append(row16)
+            for i in xrange(0, 16):
+                    rows[i].append(max(0, np.sum(np.multiply(receptivefield, convmodel2[i]))))
+        for h in xrange(0, 16):
+            featuremap2[h].append(rows[h])
     featuremap2 = np.asarray(featuremap2)
 
 
@@ -252,11 +197,12 @@ def cnn(imagePath, model, training=False):
                         maxpooled2[z][y][x] = 0
 
 
-    """3 dimensies terugbrengen naar 2 dimensies"""
-    flattened = maxpooled2.reshape(80, 5)
+    """3 dimensies terugbrengen naar 1 dimensie"""
+    flattened = maxpooled2.reshape(400)
 
 
     """fully connected layer 1"""
     fullyconnected1 = []
 
-benchmark(5)
+
+benchmark(10)
