4fe405af476904d56b91bd2c1fb5612a24069c7e
Brian Groskamp <brian.groskamp@icloud.com>
2017-01-29 16:58:34 +0100

    preprocessing code toegevoegd

--- preprocessing.py
+++ preprocessing.py
@@ -2,4 +2,12 @@ import numpy as np
 from PIL import Image
 
 def preprocess(imgPath):
-    target = Image.open(imgPath);
+    img = Image.open(imgPath)
+    img.thumbnail((32, 32), Image.ANTIALIAS)
+    container = Image.new('RGB', (32, 32), (255, 255, 255))
+    container.paste(
+    img, (int((32 - img.size[0]) / 2), int((32 - img.size[1]) / 2))
+    )
+    container.save(imgPath)
+
+preprocess('test.jpg')
